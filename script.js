fa6fb4e1b00e99cde22f3af82da1f906f870c72d
lelikone777 <2257855@users.noreply.github.com>
2020-06-20 21:40:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,24 @@
-// let num = 266219;
-// let res = 1;
-// num.toString().split('').forEach(function (el) {
-//   res *= el;
-// });
-// console.log(res);
+'use strict'
 
-// res **= 3;
+let lang;
 
-// console.log(res);
+let engDays = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
+let rusDays = 'monday, tuesday, wednesday, fourthday, friday, saturday, sunday';
 
-// res = String(res).slice(0, 2)
+if (lang = 'en') {
+  console.log(engDays);
+} else if (lang = 'ru') {
+  console.log(ruDays);
+}
 
-// console.log(res);
-// alert(res);
-
-//свое решение
-let num = 226219;
-let one = 1;
-let numStringArr = num.toString().split('');
-console.log(numStringArr); 
-console.log(numStringArr.length);
-for (let i = 0; i < numStringArr.length; i++){
-  one *= numStringArr[i]
-  }
-console.log(one);
-one **= 3;
-console.log(one);
-let oneString = String(one).split('').slice(0, 2);
-console.log(typeof oneString);
-console.log(oneString);
-
-
-
-
-// //Другое решение 
-// let mum = 266219;
-// let res = 1;
-// num.toString().split('').forEach(function (el) {
-//   res *= el;
-// });
-// console.log(res);
-
-
-
-
+switch (lang){
+  case en:
+    console.log(engDays); 
+    break
+  case ru:
+    console.log(ruDays);
+    break
+}
 
 
 
